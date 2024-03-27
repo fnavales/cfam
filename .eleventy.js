a6580dea9e7082a637a6b124a0acc3218a366085
Francisco Navarro <fran.navales@gmail.com>
2024-03-27 21:04:32 +0100

    Added image plugin to override the <img> adding loading lazy mode

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,26 @@
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
 
-  // limit filter
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin, {
+		// which file extensions to process
+		extensions: "html",
+
+		// optional, output image formats
+		formats: ["webp"],
+
+		// optional, attributes assigned on <img> override these values.
+		defaultAttributes: {
+			loading: "lazy",
+			decoding: "async",
+		},
+	});
+
+  // Function to apply a collection filter
   eleventyConfig.addFilter("limit", function(array, limit) {
     return array.slice(0, limit);
   });
 
+  // Function to order a collection
   function sortByOrder(values) {
     let vals = [...values];     // this *seems* to prevent collection mutation...
     return vals.sort((a, b) => Math.sign(a.data.order - b.data.order));
